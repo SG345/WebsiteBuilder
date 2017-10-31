23eec8c3812d99bff182f5819c1cd3072dd50240
Devesh Shetty <devesh.shetty@shopify.com>
2017-10-31 13:13:55 -0400

    fix-fix-fix

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <html>
     <head>
-      <title> Welcome to Website Builder </title>
-<link rel="stylesheet" href="style.css" >
+    	<title> Welcome to Website Builder </title>
+		<link rel="stylesheet" href="style.css" >
     </head>
 
 <body>
-<div id="header"> <h1 align="center"> <strong>Website Builder </h1> <h3 align="center">A Project by Sushrut Girdhari </h3> </div>
+	<div id="header"> <h1 align="center"> <strong>Website Builder </h1> <h3 align="center">A Project by Sushrut Girdhari </h3> </div>
 
-<div id="navbar">
-<ul id="ul">
-   <li> <a href="#"> Home </a> </li>
-   
-<li> <a href="admin/index.php"> Admin Panel </a> </li>
-<li> <a href="about.php"> About </a> </li>
-
-
-</ul>
-</div>
-<div><? php include("includes/header.php"); ?></div>
-
-<div><? php include("includes/navbar.php"); ?></div>
+	<div id="navbar">
+	<ul id="ul">
+	   <li> <a href="#"> Home </a> </li>
+	   
+	<li> <a href="admin/index.php"> Admin Panel </a> </li>
+	<li> <a href="about.php"> About </a> </li>
 
 
-<div id="main_content"> 
-<?php include("main_content.php"); ?> </div>
+	</ul>
+	</div>
+	<div><? php include("includes/header.php"); ?></div>
+
+	<div><? php include("includes/navbar.php"); ?></div>
 
 
-</div>
+	<div id="main_content"> 
+	<?php include("main_content.php"); ?> </div>
+
+
+	</div>
 
 </body>
 </html>
